d4e7c3c942f9c9b4886b1acd7f72c6c533f8af6e
JH <vjamalhall@gmail.com>
2021-07-08 16:24:03 -0400

    Add files via upload

---
+++ simple-functions/js/main.js
@@ -0,0 +1,20 @@
+//create a function that adds two numbers and alerts the sum
+function aNumber(number, number2) {
+let y = number + number2
+alert(y);
+}
+aNumber(23,34)
+
+//create a function that multiplys three numbers and console logs the product
+function mNumber(x,y,z) {
+    let num = (x * y * z)
+    console.log(num);
+}
+mNumber(4,5,23)
+
+//create a function that divides two numbers and returns the ???
+
+function dNumber(x,y) {
+        return (x / y)
+}
+// will need to call the last function
